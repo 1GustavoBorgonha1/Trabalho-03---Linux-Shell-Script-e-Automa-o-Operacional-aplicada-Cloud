ec519c8c796624bc3c5771e99bd8c0a54bd3b9d6
1GustavoBorgonha1 <gustavo13bor@gmail.com>
2026-06-14 17:29:35 -0300

    adiciona arquivo de log ao monitoramento

--- scripts/07_monitoramento.sh
+++ scripts/07_monitoramento.sh
@@ -3,13 +3,18 @@
 # Rotina de monitoramento de recursos do servidor da Clínica Veterinária
 
 monitorar_sistema() {
+
+    #Local onde fica armazenados os arquivos de log
+    LOG_FILE="/app/logs/monitoramento.log"
+    mkdir -p "$(dirname "$LOG_FILE")"
+
     # Coletar data e hora da execução
     DATA_COLETA=$(date +"%Y-%m-%d %H:%M:%S")
     
-    echo "==================================================="
-    echo "Painel de Monitoramento - Clínica Veterinária"
-    echo "Data da coleta: $DATA_COLETA"
-    echo "==================================================="
+    echo "==================================================="  | tee -a "$LOG_FILE"
+    echo "Painel de Monitoramento - Clínica Veterinária" | tee -a "$LOG_FILE"
+    echo "Data da coleta: $DATA_COLETA" | tee -a "$LOG_FILE"
+    echo "===================================================" | tee -a "$LOG_FILE"
 
     # Coleta de métricas 
     # Pega o uso de CPU limitando à primeira casa decimal
@@ -21,40 +26,40 @@ monitorar_sistema() {
     # Pega a porcentagem de uso da partição principal de disco (/)
     DISCO_USO=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
 
-    echo "Status Atual dos Recursos:"
-    echo "- Uso de CPU: ${CPU_USO}%"
-    echo "- Uso de Memória RAM: ${MEM_USO}%"
-    echo "- Uso de Disco: ${DISCO_USO}%"
+    echo "Status Atual dos Recursos:"  | tee -a "$LOG_FILE"
+    echo "- Uso de CPU: ${CPU_USO}%"  | tee -a "$LOG_FILE"
+    echo "- Uso de Memória RAM: ${MEM_USO}%"  | tee -a "$LOG_FILE"
+    echo "- Uso de Disco: ${DISCO_USO}%"  | tee -a "$LOG_FILE"
     
-    echo "---------------------------------------------------"
-    echo "Análise de Alertas:"
+    echo "---------------------------------------------------" | tee -a "$LOG_FILE"
+    echo "Análise de Alertas:" | tee -a "$LOG_FILE"
 
     # Validação de Alertas de Hardware (Limiar de 80%)
     if [ "$CPU_USO" -ge 80 ]; then
-        echo "[ALERTA] Uso de CPU muito alto no servidor!"
+        echo "[ALERTA] Uso de CPU muito alto no servidor!"  | tee -a "$LOG_FILE"
     else
-        echo "[OK] CPU operando com folga."
+        echo "[OK] CPU operando com folga." | tee -a "$LOG_FILE"
     fi
 
     if [ "$MEM_USO" -ge 80 ]; then
-        echo "[ALERTA] Uso de memória acima de 80%!"
+        echo "[ALERTA] Uso de memória acima de 80%!" | tee -a "$LOG_FILE"
     else
-        echo "[OK] Memória RAM dentro do limite seguro."
+        echo "[OK] Memória RAM dentro do limite seguro." | tee -a "$LOG_FILE"
     fi
 
     if [ "$DISCO_USO" -ge 80 ]; then
-        echo "[ALERTA] Espaço em disco crítico para os prontuários!"
+        echo "[ALERTA] Espaço em disco crítico para os prontuários!" | tee -a "$LOG_FILE"
     else
-        echo "[OK] Espaço em disco adequado."
+        echo "[OK] Espaço em disco adequado." | tee -a "$LOG_FILE"
     fi
 
     # Validação do status do Apache
     if service apache2 status | grep -q "is running"; then
-        echo "[OK] Apache em execução. Portal da clínica ONLINE."
+        echo "[OK] Apache em execução. Portal da clínica ONLINE." | tee -a "$LOG_FILE"
     else
-        echo "[ALERTA] Apache NÃO está em execução! O portal está fora do ar."
+        echo "[ALERTA] Apache NÃO está em execução! O portal está fora do ar." | tee -a "$LOG_FILE"
     fi
-    echo "==================================================="
+    echo "===================================================" | tee -a "$LOG_FILE"
 }
 
 monitorar_sistema
